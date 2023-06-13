0dc28f99d35220100bd05642c961c3937d851aef
Mackowi <dzierzenga.maciej@gmail.com>
2023-06-13 13:02:54 +0200

    added alert context, reducer, and display to usersearch component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { GithubProvider } from './components/context/github/GithubContext'
+import { AlertProvider } from './components/context/alert/AlertContext'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Alert from './components/layout/Alert'
 
 export default function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen bg-gray-600'>
+            <Navbar />
+            <main className='container mx-auto px-3 pb-12 bg-gray-600'>
+              <Alert />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
   )
 }
